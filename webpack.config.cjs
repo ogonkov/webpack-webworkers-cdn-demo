a90a9b9420df273231bea8ca76463da9d0f799b4
Alexey <lesha.ogonkov@gmail.com>
2025-11-11 12:47:13 +0000

    build: handle production mode (#2)

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,15 +1,17 @@
-module.exports = (env) => {
+module.exports = (_, argv) => {
+  const isProduction = argv.mode == "production";
+
   /** @type {import('webpack').Configuration} */
   const config = {
-    mode: env.production ? "production" : "development",
+    mode: isProduction ? "production" : "development",
     entry: ["./src/public-path.js", "./src/index.js"],
     output: {
-      filename: env.production ? "[name].[contenthash:8].js" : "js/[name].js",
-      chunkFilename: env.production
+      filename: isProduction ? "[name].[contenthash:8].js" : "js/[name].js",
+      chunkFilename: isProduction
         ? "js/[name].[contenthash:8].chunk.js"
         : "js/[name].chunk.js",
     },
-    devtool: env.production ? "source-map" : "cheap-module-source-map",
+    devtool: isProduction ? "source-map" : "cheap-module-source-map",
     devServer: {
       port: 8080,
       allowedHosts: ["foo-bar-cdn.me"],
