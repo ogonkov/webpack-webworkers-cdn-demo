f592a75ec5fd15a303895fa3ed3a8ba69542a80f
Alexey <lesha.ogonkov@gmail.com>
2025-11-25 16:48:02 +0000

    feat: add form for sending requests to worker

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { CustomWorker as Worker } from "./CustomWorker/CustomWorker.js";
+import { domReady } from "./utils/domReady.js";
 
 const app = document.getElementById("root");
+const log = document.getElementById("log");
 
 function getWorker() {
   return new Worker(new URL("./worker/worker.js", import.meta.url), {
@@ -14,10 +16,25 @@ worker.addEventListener("message", ({ data: { response } }) => {
   const p = document.createElement("p");
   p.textContent = JSON.stringify(response, null, 2);
 
-  app.appendChild(p);
+  log.appendChild(p);
 });
 
-worker.postMessage({ query: 42 });
+domReady(() => {
+  app.querySelector("form")?.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+
+    if (form) {
+      const data = new FormData(form);
+      const value = Number(data.get("value"));
+
+      if (!Number.isNaN(value) && typeof value === "number") {
+        worker.postMessage({ query: value });
+      }
+    }
+  });
+});
 
 if (module.hot) {
   module.hot.accept(
