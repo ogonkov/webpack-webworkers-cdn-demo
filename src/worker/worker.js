848ea83e0f911ae13ce82a5e3d394137e00bd541
Alexey <lesha.ogonkov@gmail.com>
2025-11-25 16:31:19 +0000

    fix: set `publicPath` in worker

--- src/worker/worker.js
+++ src/worker/worker.js
@@ -1,10 +1,16 @@
 import { en } from "../i18n/en.js";
 
-self.onmessage = async ({ data: { query } }) => {
-  const logic = await import("./worker-logic.js");
-  const response = logic.calc(query);
+self.onmessage = async ({ data: { publicPath, query } }) => {
+  if (publicPath) {
+    __webpack_public_path__ = publicPath;
+  }
 
-  self.postMessage({
-    response: `${en.answer} ${response}`,
-  });
+  if (query) {
+    const logic = await import("./worker-logic.js");
+    const response = logic.calc(query);
+
+    self.postMessage({
+      response: `${en.answer} ${response}`,
+    });
+  }
 };
